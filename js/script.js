0a2c984e6dca2d2ab19da4efc1f31de32a244ef3
Mohammed Elamassi <mohamedamassi12@gmail.com>
2024-08-15 23:29:46 +0200

    first-upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+var copyButton =document.querySelectorAll("#CopY")
+
+copyButton.forEach( function(item){
+  item.onclick=function(){
+    alert("تم نسخ النص بنجاح.")
+  }
+}
+)
+
